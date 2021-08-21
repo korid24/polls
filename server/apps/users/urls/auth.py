e3aaeb3e85e9484e0712f0b57bc6222443cd6859
Ilya Kormilkin <korid24.dev@gmail.com>
2021-08-21 20:39:16 +0700

    fix linting

--- server/apps/users/urls/auth.py
+++ server/apps/users/urls/auth.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from ..views import  RoleBasedTokenObtainPairView
+from ..views import RoleBasedTokenObtainPairView
 
 
 urlpatterns = [
